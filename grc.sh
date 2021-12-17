92a184ab780f3906ebb1afcc44e4a197241a382d
Fucking Hostile TrashPanda <83073377+OWGrand@users.noreply.github.com>
2021-12-17 07:56:36 +0200

    Update grc.sh

--- grc.sh
+++ grc.sh
@@ -15,7 +15,11 @@ fi
 ###Arguments
 #Help
 print_help () {
-	echo "Syntax: $0 [task name] <Additional args>"
-	echo "Additional args:"
-	
+echo "Syntax: $0 [repo/project name] <Additional args> <Action args>"
+echo "-- Additional args:"
+echo "[-t | task] [task name in capital letters] [Action args]"
+echo "[-b | backup] <branch name> - clones the repo and pulls the feature branch for a local safekeeping in '~/' or 'C:\Users'"
+echo "-- Adction args"
+echo "[-l | list] lists all the branches related to this task"
+echo "[-p | print]
 }
