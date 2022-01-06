3ecd1b5a64a13f2c7ef98e18480af25d1bf459f6
ventsislav.zlatkov <ventsislav.zlatkov@pronetgaming.com>
2022-01-06 20:50:02 +0200

    Debuging 6

--- git_branch_cloner.sh
+++ git_branch_cloner.sh
@@ -77,11 +77,12 @@ AUTO () {
 	###Declare an array of repos
 	declare -a PROJECTS=("vidar" "baldur" "dagur" "odin-service" "payment-service") ###Should create an array for the projects' URLs?!?
 	for PROJECTS_i in "${PROJECTS[@]}"; do
-	git clone https://git.pronetdev.com/sbbackend/$PROJECTS_i.git ~/GIT/ ###I HAVE TO CHECK THE CORRECT URL NAMING CONVENTION!
-	git --git-dir=~/GIT/$PROJECTS_i/.git
+	git clone https://git.pronetdev.com/sbbackend/$PROJECTS_i.git ~/GIT/$PROJECTS_i ###I HAVE TO CHECK THE CORRECT URL NAMING CONVENTION!
+	git --git-dir="~/GIT/$PROJECTS_i/.git"
 	git fetch -a
 	git pull
-	git checkout -b develop-$DATE_DEV RC-$DATE_RC
+	git checkout RC-$DATE_RC
+	git branch develop-$DATE_DEV
 	done
 }
 
