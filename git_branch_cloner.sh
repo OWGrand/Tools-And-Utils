56409030d42bc5b86bf60b7eddc817c0ba2a1301
owgrand <ventsi.zlatkov@gmail.com>
2022-01-10 12:25:46 +0200

    test

--- git_branch_cloner.sh
+++ git_branch_cloner.sh
@@ -3,15 +3,16 @@
 ### Version	1.0  @V.Zlatkov a.k.a - Xerxes###
 #############################################
 ##### GBC - Git branch cloner script
-##### V.1.0
+##### V.1.1
 ##### v.0.2 - Added a DIRCHECK funct that prevents the script to fail if the ~/GIT dir doesn't exist
 ##### V.0.3 - Fixed the "$DATE" var to format YY (ex.:22) instead of YYYY (ex.: 2022)
 ##### V.0.4 - Ready to be used as a cronjob util to create "develop" from RC branches
 ##### V.0.5 - Debuging done - Translates into version "V.1.0"
 ##### V.1.0
+##### V.1.1 - Redone so you can use the automode to create diferent branches with the same script
 
 # THOSE ARE THE VARIABLES FOR THE COLORS:
-GREEN="\e[32m"
+GREEN=`\e[32m`
 RED="\e[31m"
 ENDCOLOR="\e[0m"
 
@@ -34,7 +35,7 @@ fi
 
 ###Check dependencies
 #Check if git is present on the system
-DEP_CHECK () {
+DEPCHECK () {
 git_check=$(whereis git | cut -d' ' -f2)
 if [ ! -x "$git_check" ];
 then
@@ -71,33 +72,79 @@ fi
 #Help
 PRINT_HELP () {
 echo "Syntax: $0: "
-echo "[-a | auto] - automatic mode created for the purpose to be used in crontab as a weekly job"
+echo "[-D | DEV] - automatic mode created for the purpose to be used in crontab as a weekly job to create dev from RC- branch"
+echo "[-R | RC] - automatic mode created for the purpose to be used in crontab as a weekly job to create RC from master branch"
 echo "[-m| manual] - <name of the branch you want to clone> <name of the new branch you want to create>"
 }
 
-AUTO () {
+AUTO_DEV () {
 	DIRCHECK
-	rm -rf ~/GIT/$PROJECTS_i
 	###Declare an array of repos
-	declare -a PROJECTS=("vidar" "baldur" "dagur" "odin-service" "payment-service") ###Should create an array for the projects' URLs?!?
+	declare -a PROJECTS=("repo1" "repo2" "repo3" "repo4" "repo5") 
+	rm -rf ~/GIT/$PROJECTS_i
 	for PROJECTS_i in "${PROJECTS[@]}"; do
-	git clone https://git.pronetdev.com/sbbackend/$PROJECTS_i.git ~/GIT/$PROJECTS_i ###I HAVE TO CHECK THE CORRECT URL NAMING CONVENTION!
-	#git --git-dir="~/GIT/$PROJECTS_i/.git"
+	git clone https://git.repo.com/$PROJECTS_i.git ~/GIT/$PROJECTS_i 
 	cd ~/GIT/$PROJECTS_i/
 	git fetch -a
 	git pull
 	git checkout RC-$DATE_RC
 	git branch develop-$DATE_DEV
+	git push -u origin develop-$DATE_DEV
+	done
+}
+
+AUTO_RC () {
+	DIRCHECK
+	###Declare an array of repos
+	declare -a PROJECTS=("repo1" "repo2" "repo3" "repo4" "repo5") 
+	rm -rf ~/GIT/$PROJECTS_i
+	for PROJECTS_i in "${PROJECTS[@]}"; do
+	git clone https://git.repo.com/$PROJECTS_i.git ~/GIT/$PROJECTS_i 
+	cd ~/GIT/$PROJECTS_i/
+	git fetch -a
+	git pull
+	git checkout master
+	git branch RC-$DATE_RC
+	git push -u origin develop-$DATE_RC
 	done
 }
 
 MANUAL () {
-echo TBA
+	echo -e "${GREEN}This is the manual mode, please provide the following values:${ENDCOLOR}"
+	echo
+	echo
+	read -p "$cr${RED}Please provide the repo's name: ${ENDCOLOR}$cr" repo
+	echo $cr
+	read -p "$cr${RED}Please provide the name of the branch you want to use as a origin : ${ENDCOLOR}$cr" branch_or
+	echo $cr
+	read -p "$cr${RED}Please provide the name of the branch you want to be created as a clone : ${ENDCOLOR}$cr" branch_clone
+	echo $cr
+	if [ ! $repo ];then
+		git fetch -a
+		git checkout $branch_or
+		git pull
+		git branch $branch_clone
+		git pull
+		git push -u origin $branch_clone
+	else
+		DIRCHECK
+		rm -rf ~/GIT/$repo
+		git clone https://git.repo.com/$repo.git ~/GIT/$repo
+		cd ~/GIT/$repo/
+		git fetch -a
+		git checkout $branch_or
+		git pull
+		git branch $branch_clone
+		git pull
+		git push -u origin $branch_clone
+	fi
 }
 
 ###Arg phrasing
 case "${1}" in
-	-a|auto) AUTO
+	-D|DEV) AUTO_DEV
+		;;
+	-R|RC) AUTO_RC
 		;;
 	-m|manual) MANUAL
 		;;
